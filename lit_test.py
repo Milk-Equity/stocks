b227a1aedb4743ebcffb580789fec209216e0d91
Lucas <lucaswb15@gmail.com>
2021-12-04 20:57:00 -0500

    t

--- lit_test.py
+++ lit_test.py
@@ -5,26 +5,33 @@ from plotly.subplots import make_subplots
 from ta.trend import MACD
 from ta.momentum import StochasticOscillator # MACD
 import streamlit as st
-from datetime import datetime as dt
 import datetime
+import numpy as np
+import yahoo_fin.stock_info as si
+import plotly.figure_factory as ff
 
+#Page Config
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 #Get Ticker
 ticker = st.sidebar.text_input('Symbol', value='AAPl', max_chars=4)
 
 #Get Date
-#today = datetime.datetime.strptime(date, "%m/%d/%Y")
-#today = datetime.date.today()
 date = datetime.date.today()
-d = st.sidebar.date_input("Starting Date", datetime.date(2019, 7, 6), max_value=date)
+#Sidebar
+d = st.sidebar.date_input("Starting Date", datetime.date(2012, 1, 6), max_value=date)
 
 
 #Download data from Date and Ticker
-df = yf.download(ticker, auto_adjust=True, start=d)
-                 #period = '3Y')
+df = yf.download(ticker, auto_adjust=True, start=d, period = '1Y')
 
 
-
-# removing all empty dates
+#Data Cleaning DON'T DELETE
+#removing all empty dates
 # build complete timeline from start date to end date
 dt_all = pd.date_range(start=df.index[0],end=df.index[-1])
 
@@ -43,13 +50,23 @@ fig.add_trace(go.Candlestick(x=df.index,
                              low=df['Low'],
                              close=df['Close'],
                              showlegend=False))
-fig.update_layout(title=ticker)
+#fig.update_layout(title=ticker)
+
+#Fundamentals
+fund = si.get_quote_table(ticker, dict_result=False)
+fund.columns = ['Fundamentals ', 'Data']
+fig_fund =  ff.create_table(fund)
 
 
 
+#Starting Layout
+col1, col2 = st.columns([1, 3])
 
 
+with col1:
+    col1.subheader("Stock Fundamentals")
+    st.plotly_chart(fig_fund, use_container_width=True)
 
-
-st.plotly_chart(fig, use_container_width=False)
-
+with col2:
+    st.subheader(ticker)
+    st.plotly_chart(fig, use_container_width=True)
