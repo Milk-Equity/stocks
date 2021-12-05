a225a5fe48cec5976cdfd595c6e70eef9b092a3b
Lucas <lucaswb15@gmail.com>
2021-12-05 09:32:00 -0500

    t

--- lit_test.py
+++ lit_test.py
@@ -6,20 +6,19 @@ from ta.trend import MACD
 from ta.momentum import StochasticOscillator # MACD
 import streamlit as st
 import datetime
-import numpy as np
 import yahoo_fin.stock_info as si
 import plotly.figure_factory as ff
 
 #Page Config
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="Milk Equity",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 #Get Ticker
-ticker = st.sidebar.text_input('Symbol', value='AAPl', max_chars=4)
-
+ticker = st.sidebar.text_input('Symbol', value='AAPL', max_chars=4)
+ticker = ticker.upper()
 #Get Date
 date = datetime.date.today()
 #Sidebar
@@ -27,7 +26,8 @@ d = st.sidebar.date_input("Starting Date", datetime.date(2012, 1, 6), max_value=
 
 
 #Download data from Date and Ticker
-df = yf.download(ticker, auto_adjust=True, start=d, period = '1Y')
+df = yf.download(ticker, auto_adjust=True, start=d)
+                 #, period = '1Y')
 
 
 #Data Cleaning DON'T DELETE
@@ -47,13 +47,9 @@ df['MA30'] = df['Close'].rolling(window=30).mean()
 
 
 
-fig = go.Figure()
-# add subplot properties when initiliazing fig variable
-fig = make_subplots(rows=4, cols=1, shared_xaxes=True,
-                    vertical_spacing=0.01,
-                    row_heights=[0.5,0.1,0.2,0.2])
-
-# Plot OHLC on 1st subplot (using the codes from before)
+# first declare an empty figure
+fig = make_subplots(rows=2,row_heights=[0.5,0.1])
+# add OHLC trace
 fig.add_trace(go.Candlestick(x=df.index,
                              open=df['Open'],
                              high=df['High'],
@@ -70,7 +66,6 @@ fig.add_trace(go.Scatter(x=df.index,
                          line=dict(color='orange', width=2),
                          name='MA 60'))
 
-
 # Plot volume trace on 2nd row
 colors = ['green' if row['Open'] - row['Close'] >= 0
           else 'red' for index, row in df.iterrows()]
@@ -78,31 +73,15 @@ fig.add_trace(go.Bar(x=df.index,
                      y=df['Volume'],
                      marker_color=colors
                     ), row=2, col=1)
-
-
-
-# update layout by changing the plot size, hiding legends & rangeslider, and removing gaps between dates
+#Layout stuff
 fig.update_layout(height=900, width=1200,
                   showlegend=False,
-                  xaxis_rangeslider_visible=False,
-                  xaxis_rangebreaks=[dict(values=dt_breaks)])
+                  xaxis_rangeslider_visible=False,)
+                  #xaxis_rangebreaks=[dict(values=dt_breaks)])
 
 # update y-axis label
 fig.update_yaxes(title_text="Price", row=1, col=1)
 fig.update_yaxes(title_text="Volume", row=2, col=1)
-fig.update_yaxes(title_text="MACD", showgrid=False, row=3, col=1)
-fig.update_yaxes(title_text="Stoch", row=4, col=1)
-######################################################
-
-
-# add OHLC trace
-#fig.add_trace(go.Candlestick(x=df.index,
-#                             open=df['Open'],
-#                             high=df['High'],
-#                             low=df['Low'],
-#                             close=df['Close'],
-#                             showlegend=False))
-#fig.update_layout(title=ticker)
 
 #Fundamentals
 fund = si.get_quote_table(ticker, dict_result=False)
