1496843f5015221bbf8201aab82b30b832a513c2
Lucas <lucaswb15@gmail.com>
2021-12-22 20:59:11 -0500

    stuff
    stuff

--- dash_t2.py
+++ dash_t2.py
@@ -1,43 +1,42 @@
 import pandas as pd
 import yfinance as yf
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from ta.trend import MACD
-from ta.momentum import StochasticOscillator  # MACD
+#from plotly.subplots import make_subplots
+#from ta.trend import MACD
+#from ta.momentum import StochasticOscillator  # MACD
 import streamlit as st
 import datetime
-import yahoo_fin.stock_info as si
-import plotly.figure_factory as ff
-import plotly.express as px
 from dateutil.relativedelta import relativedelta
 
-# Page Config
-st.set_page_config(
-    page_title="Milk Equity",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+import yahoo_fin.stock_info as si
+import plotly.figure_factory as ff
+#import plotly.express as px
+
+
+
 # Get Ticker
-symbol = st.sidebar.text_input('Symbol', value='AAPL', max_chars=4)
+symbol = st.sidebar.text_input('Symbol', value='MSFT', max_chars=4)
 symbol = symbol.upper()
 # Get Date
 todays_date = datetime.date.today()
+one_day = datetime.date.today() - relativedelta(days=1)
+five_days = datetime.date.today() - relativedelta(days=5)
+one_month = datetime.date.today() - relativedelta(months=1)
+three_months = datetime.date.today() - relativedelta(months=3)
+six_months = datetime.date.today() - relativedelta(months=6)
 yr_ago = datetime.date.today() - relativedelta(years=1)
 two_yrs_ago = datetime.date.today() - relativedelta(years=2)
 three_yrs_ago = datetime.date.today() - relativedelta(years=3)
+five_yrs_ago = datetime.date.today() - relativedelta(years=5)
 
 # Sidebar
-date = st.sidebar.date_input("Starting Date", datetime.date(2020, 1, 6), max_value=todays_date)
-
-# Download data from Date and Ticker
+#date = st.sidebar.date_input("Starting Date", datetime.date(2019,1,5), max_value=todays_date)
+#ytd = st.sidebar.button('YTD')
 
 @st.cache
 def get_data(symbol):
     df = yf.download(symbol, auto_adjust=True)
-    # Data Cleaning DON'T DELETE
-    # removing all empty dates
-    # build complete timeline from start date to end date
+    df['Daily Change'] = df['Close'] - df['Open']
     dt_all = pd.date_range(start=df.index[0], end=df.index[-1])
 
     # retrieve the dates that ARE in the original datset
@@ -45,21 +44,33 @@ def get_data(symbol):
 
     # define dates with missing values
     dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
-
-    # Daily Change
-    df['Daily Change'] = df['Close'] - df['Open']
     return df
-
-df = get_data(symbol).loc[date:todays_date]
-
-df.reset_index(level=0, inplace=True)
-
-
-
-
-
+df = get_data(symbol)
+df = df.loc[five_yrs_ago:todays_date]
+if st.sidebar.button('1 Day'):
+    df = df.loc[one_day:todays_date]
+if st.sidebar.button('5 Days'):
+    df = df.loc[five_days:todays_date]
+if st.sidebar.button('1 Month'):
+    df = df.loc[one_month:todays_date]
+if st.sidebar.button('3 Months'):
+    df = df.loc[three_months:todays_date]
+if st.sidebar.button('6 Months'):
+    df = df.loc[six_months:todays_date]
 if st.sidebar.button('1 Year'):
     df = df.loc[yr_ago:todays_date]
+if st.sidebar.button('2 Years'):
+    df = df.loc[two_yrs_ago:todays_date]
+if st.sidebar.button('3 Years'):
+    df = df.loc[three_yrs_ago:todays_date]
+
+
+
+
+
+# build complete timeline from start date to end date
+
+
 
 def colors(df):
     oldest = df['Close'].iloc[0] #0 is first -1 is last
@@ -70,11 +81,16 @@ def colors(df):
     else:
         return 'red'
 
+
+
+
 #Main Chart
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df.Date, y=df.Close, mode='lines', fill='tozeroy', line={'color': colors(df)}))
+fig.add_trace(go.Scatter(x=df.index, y=df.Close, mode='lines', fill='tozeroy', line={'color': colors(df)}))
+
 
 # Fundamentals
+@st.cache
 def fundamentals(symbol):
     fund = si.get_quote_table(symbol, dict_result=False)
     fund.columns = ['Fundamentals ', 'Data']
@@ -83,14 +99,14 @@ def fundamentals(symbol):
 
 
 # Starting Layout
-col1, col2 = st.columns([1, 3])
+col1, col2 = st.columns([1,4])
 
 with col1:
     col1.subheader("Stock Fundamentals")
     st.plotly_chart(fundamentals(symbol), use_container_width=True)
 
 with col2:
+    st.button('one')
+    st.button('two')
     st.subheader(symbol)
-
-
     st.plotly_chart(fig, use_container_width=True)
