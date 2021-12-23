2999a555485ae7a4497efab67e6abff47a2cc5a7
Lucas <lucaswb15@gmail.com>
2021-12-22 20:42:44 -0500

    t

--- dash_test.py
+++ dash_test.py
@@ -19,9 +19,15 @@ symbol = st.sidebar.text_input('Symbol', value='MSFT', max_chars=4)
 symbol = symbol.upper()
 # Get Date
 todays_date = datetime.date.today()
+one_day = datetime.date.today() - relativedelta(days=1)
+five_days = datetime.date.today() - relativedelta(days=5)
+one_month = datetime.date.today() - relativedelta(months=1)
+three_months = datetime.date.today() - relativedelta(months=3)
+six_months = datetime.date.today() - relativedelta(months=6)
 yr_ago = datetime.date.today() - relativedelta(years=1)
 two_yrs_ago = datetime.date.today() - relativedelta(years=2)
 three_yrs_ago = datetime.date.today() - relativedelta(years=3)
+five_yrs_ago = datetime.date.today() - relativedelta(years=5)
 
 # Sidebar
 #date = st.sidebar.date_input("Starting Date", datetime.date(2019,1,5), max_value=todays_date)
@@ -31,31 +37,42 @@ three_yrs_ago = datetime.date.today() - relativedelta(years=3)
 def get_data(symbol):
     df = yf.download(symbol, auto_adjust=True)
     df['Daily Change'] = df['Close'] - df['Open']
+    dt_all = pd.date_range(start=df.index[0], end=df.index[-1])
+
+    # retrieve the dates that ARE in the original datset
+    dt_obs = [d.strftime("%Y-%m-%d") for d in pd.to_datetime(df.index)]
+
+    # define dates with missing values
+    dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
     return df
 df = get_data(symbol)
-
-
+df = df.loc[five_yrs_ago:todays_date]
+if st.sidebar.button('1 Day'):
+    df = df.loc[one_day:todays_date]
+if st.sidebar.button('5 Days'):
+    df = df.loc[five_days:todays_date]
+if st.sidebar.button('1 Month'):
+    df = df.loc[one_month:todays_date]
+if st.sidebar.button('3 Months'):
+    df = df.loc[three_months:todays_date]
+if st.sidebar.button('6 Months'):
+    df = df.loc[six_months:todays_date]
 if st.sidebar.button('1 Year'):
     df = df.loc[yr_ago:todays_date]
+if st.sidebar.button('2 Years'):
+    df = df.loc[two_yrs_ago:todays_date]
+if st.sidebar.button('3 Years'):
+    df = df.loc[three_yrs_ago:todays_date]
 
-
-
-#df = df.loc[three_yrs_ago:todays_date]
 
 
 
 
 # build complete timeline from start date to end date
-dt_all = pd.date_range(start=df.index[0], end=df.index[-1])
-
-# retrieve the dates that ARE in the original datset
-dt_obs = [d.strftime("%Y-%m-%d") for d in pd.to_datetime(df.index)]
-
-# define dates with missing values
-dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
 
 
-def colors2(df):
+
+def colors(df):
     oldest = df['Close'].iloc[0] #0 is first -1 is last
     today = df['Close'].iloc[-1]
     color_num = today - oldest
@@ -65,13 +82,6 @@ def colors2(df):
         return 'red'
 
 
-#Colors for chart
-def colors(df):
-    dc = df['Daily Change'].iloc[-1]
-    if dc > 0:
-        return 'green'
-    else:
-        return 'red'
 
 
 #Main Chart
@@ -80,6 +90,7 @@ fig.add_trace(go.Scatter(x=df.index, y=df.Close, mode='lines', fill='tozeroy', l
 
 
 # Fundamentals
+@st.cache
 def fundamentals(symbol):
     fund = si.get_quote_table(symbol, dict_result=False)
     fund.columns = ['Fundamentals ', 'Data']
@@ -95,7 +106,7 @@ with col1:
     st.plotly_chart(fundamentals(symbol), use_container_width=True)
 
 with col2:
+    st.button('one')
+    st.button('two')
     st.subheader(symbol)
-
-
     st.plotly_chart(fig, use_container_width=True)
